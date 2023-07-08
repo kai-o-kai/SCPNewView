89583fe9125b7e45b47d01b1b96ac6b351f05040
Connor William Roy Egan <111745780+kai-o-kai@users.noreply.github.com>
2023-07-08 17:31:46 +1000

    Update ISerializer.cs

--- Assets/Scripts/Saving/ISerializer.cs
+++ Assets/Scripts/Saving/ISerializer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace SCPNewView.Saving.Serialization {
     public interface ISerializer {
-// TODO : This interface should take in objects not strings so the interface can be used with other serializers like xml or binary.
         object Serialize(object toSerialize);
         object Deserialize<T>(object toDeserialize);
     }
