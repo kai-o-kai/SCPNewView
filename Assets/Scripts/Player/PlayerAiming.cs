254aa84735f363fbbfca25655d41c7a887680b8e
Connor William Roy Egan <connorwilliamroy.egan@gmail.com>
2023-07-08 14:57:44 +1000

    Create PlayerAiming.cs and adjust lighting
    Lighting looks pretty good. I think I'll use 2D lights. However, I'm going to keep around the scripts for the other lighting system incase I decide to change.

---
+++ Assets/Scripts/Player/PlayerAiming.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SCPNewView {
+    public class PlayerAiming : MonoBehaviour {
+        [SerializeField] Camera cam;
+
+        void Awake() {
+            cam = cam == null ? Camera.main : cam;    
+        }
+        void Update() {
+            transform.rotation = Quaternion.Euler(0f, 0f, GetAngleToCamera());    
+        }
+
+        // TODO : This needs to work with controllers too.
+        float GetAngleToCamera() {
+            Vector2 mousePosScreenSpace = Mouse.current.position.ReadValue();
+            Vector2 mousePosWorldSpace = cam.ScreenToWorldPoint(mousePosScreenSpace);
+            Vector2 dirToMouse = mousePosWorldSpace - (Vector2)transform.position;
+            float angle = Mathf.Atan2(dirToMouse.y, dirToMouse.x) * Mathf.Rad2Deg - 90f;
+            return angle;
+        }
+    }
+}
